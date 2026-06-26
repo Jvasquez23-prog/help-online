18f74aa8034af10c7946d51a2936aa952f872bb9
Julio <vasquezjuliocr@gmail.com>
2026-06-26 02:08:36 -0600

    establecí la conexión a la base de datos

---
+++ src/config/db.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import mysql from 'mysql2';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: '',
+  user: '',
+  password: '',
+  database: ''
+});
+
+db.connect((error) => {
+  if (error) {
+    console.error('Error de conexión MySQL: ', error);
+  } else {
+    console.log('!Conexión Existosa!')
+  }
+});
+
+app.get("/Administrador", (request, response) => {
+  db.query("SELECT * FROM Administradores", (err, result) => {
+    if (err) {
+      console.error('Error en la consulta:', err);
+      return res.json({ error: err });
+    }
+    
+    res.json(result);
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});

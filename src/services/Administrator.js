ada65e29ec61f36ce9f4fdcce165a1ace6e5c333
Julio <vasquezjuliocr@gmail.com>
2026-06-26 02:12:42 -0600

    añadí servicio de obtención de datos de administradores

---
+++ src/services/Administrator.js
@@ -0,0 +1,14 @@
+export async function getData() {
+  const url = 'http://localhost:5000/Administrador';
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  } 
+}
